904cdb5ffd8f0138e4f35d4d938120555e1bcf99
Richard Otto <richardsoeuzin@gmail.com>
2020-08-23 07:18:12 -0300

    fix(client): partial fix on tooltips not showing up
    Currently tooltips show up until React updates their parent components

--- client/src/components/Tooltip/index.js
+++ client/src/components/Tooltip/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useRef, cloneElement } from 'react';
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 
 // A pretty redundant component built so tippy.js's default CSS can be imported once instead of
 // every time its tooltip component is used
-function Tooltip(props){
-  return <Tippy {...props} />;
+function Tooltip({ children, ...props }){
+
+  const ref = useRef();
+
+  return (
+    <>
+      {children && cloneElement(children, { ref })}
+      <Tippy reference={ref} {...props} />
+    </>
+  );
 };
 
 export default Tooltip;
