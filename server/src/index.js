8e4c54a5a2f9b0ede4df7275c2c45938c76226c3
Richard Otto <richardsoeuzin@gmail.com>
2020-08-28 09:04:07 -0300

    fix(server): fixed 404 not found response

--- server/src/index.js
+++ server/src/index.js
@@ -54,7 +54,7 @@ app.post('/project/move', (req, res) => {
 });
 
 app.use((req, res, next) => {
-  res.status(404).send("404 Internet Error: Invalid API route");
+  res.status(404).send("404 Not Found Error: Invalid API route");
 });
 
 app.listen(8000, () => {
