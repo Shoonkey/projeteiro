e686a53fc51308d7318d0b734dddd0592238e80b
Richard Otto <richardsoeuzin@gmail.com>
2020-09-11 03:27:05 -0300

    feat(client): changed the default scrollbar on project track

--- client/src/components/ProjectTrack/styles.js
+++ client/src/components/ProjectTrack/styles.js
@@ -1,6 +1,25 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  
   display: flex;
   overflow-x: auto;
+
+  scrollbar-width: thin;
+  scrollbar-color: #2b2b2b #d2d2d2;
+
+  padding-bottom: .5em;
+
+  &::-webkit-scrollbar { width: 5px; height: 6px; }
+  &::-webkit-scrollbar-track {
+    background: #4b4b4b;
+    border-radius: 6px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #8d8d8d;
+    border-radius: 6px;
+  }
+
+
 `;
